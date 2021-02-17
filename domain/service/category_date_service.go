b4c37c05311b9964512a6281f70227348dc970a8
xzlinux@163.com <xzlinux@163.com>
2021-02-17 13:08:55 +0800

    add domain ok

--- domain/service/category_date_service.go
+++ domain/service/category_date_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/xzlinux/category/domain/model"
+	"github.com/xzlinux/category/domain/respository"
 )
 
 type ICategoryDataService interface {
@@ -17,4 +18,35 @@ type ICategoryDataService interface {
 
 type CategoryDataService struct {
 	CategoryRepository respository.ICategoryRepository
+}
+
+func NewCategoryDataService(categoryRepository respository.ICategoryRepository) ICategoryDataService {
+	return &CategoryDataService{categoryRepository}
+}
+
+func (u *CategoryDataService) AddCategory(category *model.Category) (int64, error) {
+	return u.CategoryRepository.CreateCategory(category)
+}
+func (u *CategoryDataService) DeleteCategory(categoryID int64) error {
+	return u.CategoryRepository.DeleteCategoryByID(categoryID)
+}
+func (u *CategoryDataService) UpdateCategory(category *model.Category) error {
+	return u.CategoryRepository.UpdateCategory(category)
+}
+func (u *CategoryDataService) FindCategoryByID(categoryID int64) (*model.Category, error) {
+	return &u.CategoryRepository.FindCategoryByID(categoryID)
+}
+func (u *CategoryDataService) FindAllCategory() ([]model.Category, error) {
+	return u.CategoryRepository.FindAll()
+}
+
+func (u *CategoryDataService) FindCategoryByName(categoryName string) (*model.Category, error) {
+	return &u.CategoryRepository.FindCategoryByName(categoryName)
+}
+
+func (u *CategoryDataService) FindCategoryByLevel(level uint32) ([]model.Category, error) {
+	return u.CategoryRepository.FindCategoryByLevel(level)
+}
+func (u *CategoryDataService) FindCategoryByParent(parent int64) ([]model.Category, error) {
+	return u.CategoryRepository.FindCategoryByParent(parent)
 }
