d4cdf372a61547fd76a13f4ff35ee1af2ee8387b
xzlinux@163.com <xzlinux@163.com>
2021-02-17 15:53:00 +0800

    fix

--- common/config.go
+++ common/config.go
@@ -4,7 +4,7 @@ import (
 	"strconv"
 
 	"github.com/micro/go-micro/v2/config"
-	"github.com/micro/go-plugins/config/source/consul"
+	consul "github.com/micro/go-plugins/config/source/consul/v2"
 )
 
 func GetConsulConfig(host string, port int64, prefix string) (config.Config, error) {
