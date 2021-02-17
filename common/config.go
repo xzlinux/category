5154add5e28d41d6c54ca74bb6f88cfb66188eb4
xzlinux@163.com <xzlinux@163.com>
2021-02-17 14:21:11 +0800

    update common

---
+++ common/config.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"strconv"
+
+	"github.com/micro/go-micro/v2/config"
+	"github.com/micro/go-plugins/config/source/consul"
+)
+
+func GetConsulConfig(host string, port int64, prefix string) (config.Config, error) {
+	consulSource := consul.NewSource(
+		consul.WithAddress(host+":"+strconv.FormatInt(port, 10)),
+		consul.WithPrefix(prefix),
+		consul.StripPrefix(true),
+	)
+	config, err := config.NewConfig()
+	if err != nil {
+		return config, err
+	}
+	err = config.Load(consulSource)
+	return config, err
+
+}
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/micro/go-micro v1.18.0
+	github.com/micro/go-micro/v2 v2.9.1 // indirect
 	google.golang.org/protobuf v1.25.0
 )
