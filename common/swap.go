92cecca19e81897d25e0ac4725f929b424f7ee1b
xzlinux@163.com <xzlinux@163.com>
2021-02-18 19:29:02 +0800

    te

--- common/swap.go
+++ common/swap.go
@@ -2,23 +2,13 @@ package common
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 func SwapTo(request, category interface{}) (err error) {
-	fmt.Printf("request type is:", request)
-	fmt.Printf("request type is: %T\n", request)
 	dateByte, err := json.Marshal(request)
-	fmt.Printf("request:%s", dateByte)
-
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
-	err = json.Unmarshal(dateByte, category)
-	fmt.Println("datrt")
-	fmt.Println(category)
-	fmt.Println("end")
 	dateByte, err = json.Marshal(category)
-	fmt.Printf("request222:", string(dateByte))
 	return err
 }
